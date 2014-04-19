e2f7179e5c777d69e172e08806744da13c914e65
AmandaLG <amandalg@gmail.com>
2014-04-18 19:11:04 -0700

    Script clean up.

--- install/davix-apt-get-install-v1.0.sh
+++ install/davix-apt-get-install-v1.0.sh
@@ -6,7 +6,7 @@
 ##                                                ##
 ## Raffael Marty, Amanda Gellhouse, Edward McCabe ##
 ##                                                ##
-## DEVELOPMENT v1.1 17 APR 2014                   ##
+## DEVELOPMENT v1.1.02 18 APR 2014                ##
 ####################################################
 
 apt-get update
@@ -14,52 +14,26 @@ apt-get -y -u upgrade
 apt-key adv --recv-keys --keyserver keyserver.ubuntu.com 16126D3A3E5C1192
 apt-get -y remove ubuntu-minimal
 
+## Primary Packages
 apt-get -y install apache2-mpm-prefork
-apt-get -y install argus-server
-apt-get -y install bison
 apt-get -y install chaosreader
-apt-get -y install cmake
-apt-get -y install cvs
 apt-get -y install default-jre
-apt-get -y install dh-apparmor
 apt-get -y install dns-browse
 apt-get -y install dnstop
-apt-get -y install dpatch
 apt-get -y install etherape
-apt-get -y install expat
 apt-get -y install exuberant-ctags
-apt-get -y install flex
-apt-get -y install fonts-droid
-apt-get -y install fonts-horai-umefont
-apt-get -y install fonts-unfonts-core
 apt-get -y install ftpd
-apt-get -y install geeqie
 apt-get -y install geoip-database
-apt-get -y install geoip-bin 
-apt-get -y install gfortran
 apt-get -y install ggobi
-apt-get -y install gnome-desktop-environment
-apt-get -y install gnome-exe-thumbnailer
 apt-get -y install gnuplot
 apt-get -y install googleearth-package
 apt-get -y install graphviz
-apt-get -y install gresolver
 apt-get -y install gwhois
 apt-get -y install htcheck
 apt-get -y install htdig
-apt-get -y install htdig-doc
-apt-get -y install html2text
-apt-get -y install icedtea-6-jre-cacao
-apt-get -y install icedtea-6-jre-jamvm
-apt-get -y install icedtea-netx
-apt-get -y install java-common
 apt-get -y install java3ds-fileloader
 apt-get -y install joe
-apt-get -y install krb5-admin-server
-apt-get -y install krb5-user
 apt-get -y install lftp
-apt-get -y install littler
-apt-get -y install lmodern
 apt-get -y install lynx
 apt-get -y install mrtg
 apt-get -y install mysql-client
@@ -73,8 +47,63 @@ apt-get -y install nfdump
 apt-get -y install ngrep
 apt-get -y install nmap
 apt-get -y install ntp
-apt-get -y install nvi
 apt-get -y install octave
+apt-get -y install openssh-server
+apt-get -y install p0f
+apt-get -y install pads
+apt-get -y install picviz
+apt-get -y install ploticus
+apt-get -y install prads
+apt-get -y install prelude-manager
+apt-get -y install python
+apt-get -y install python-geoip
+apt-get -y install python-gnuplot
+apt-get -y install rrdtool
+apt-get -y install sagan
+apt-get -y install snort
+apt-get -y install socat
+apt-get -y install subversion
+apt-get -y install syslog-ng
+apt-get -y install tcpflow
+apt-get -y install tcpreplay
+apt-get -y install tcpslice
+apt-get -y install tcpstat
+apt-get -y install tcpxtract
+apt-get -y install tor
+apt-get -y install tshark
+apt-get -y install tulip
+apt-get -y install tulip-doc
+apt-get -y install unrar
+apt-get -y install vim
+apt-get -y install winbind
+apt-get -y install wine
+apt-get -y install wireshark
+apt-get -y install zenmap
+
+## Secondary Packages
+apt-get -y install bison
+apt-get -y install cmake
+apt-get -y install cvs
+apt-get -y install dh-apparmor
+apt-get -y install dpatch
+apt-get -y install expat
+apt-get -y install flex
+apt-get -y install fonts-unfonts-core
+apt-get -y install geeqie
+apt-get -y install geoip-bin 
+apt-get -y install gfortran
+apt-get -y install gnome-desktop-environment
+apt-get -y install gresolver
+apt-get -y install htdig-doc
+apt-get -y install html2text
+apt-get -y install icedtea-6-jre-cacao
+apt-get -y install icedtea-6-jre-jamvm
+apt-get -y install icedtea-netx
+apt-get -y install krb5-admin-server
+apt-get -y install krb5-user
+apt-get -y install littler
+apt-get -y install lmodern
+apt-get -y install nvi
 apt-get -y install octave-image
 apt-get -y install octave-linear-algebra
 apt-get -y install octave-mapping
@@ -84,9 +113,6 @@ apt-get -y install octave-splines
 apt-get -y install octave-tsa
 apt-get -y install openjdk-6-jre-lib
 apt-get -y install openjdk-6-jdk
-apt-get -y install openssh-server
-apt-get -y install p0f
-apt-get -y install pads
 apt-get -y install php-auth-sasl 
 apt-get -y install php-mail
 apt-get -y install php-mail-mime
@@ -94,16 +120,7 @@ apt-get -y install php-net-socket
 apt-get -y install php5-curl
 apt-get -y install php5-dev 
 apt-get -y install php5-geoip
-apt-get -y install php5-mysql
-apt-get -y install picviz
-apt-get -y install ploticus
-apt-get -y install prads
-apt-get -y install prelude-manager
-apt-get -y install python-cairo
 apt-get -y install python-central
-apt-get -y install python
-apt-get -y install python-geoip
-apt-get -y install python-gnuplot
 apt-get -y install python-pysqlite2
 apt-get -y install python-pyx
 apt-get -y install python-scapy
@@ -111,42 +128,18 @@ apt-get -y install python-scour
 apt-get -y install python-visual
 apt-get -y install qtoctave
 apt-get -y install recode
-apt-get -y install rrdtool
 apt-get -y install ruby-hoe
 apt-get -y install ruby-net-ssh-multi
 apt-get -y install ruby-rubyforge
-apt-get -y install sagan
 apt-get -y install sharutils
-apt-get -y install snort
-apt-get -y install socat
 apt-get -y install sqlite3
-apt-get -y install subversion
 apt-get -y install swig
-apt-get -y install syslog-ng
-apt-get -y install tcpflow
-apt-get -y install tcpreplay
-apt-get -y install tcpslice
-apt-get -y install tcpstat
-apt-get -y install tcpxtract
 apt-get -y install texlive-binaries
 apt-get -y install terminator
 apt-get -y install tk 
-apt-get -y install tor
-apt-get -y install tor-geoipdb
-apt-get -y install tshark
 apt-get -y install ttf-bitstream-vera
-apt-get -y install ttf-dejavu
-apt-get -y install ttf-mscorefonts-installer
 apt-get -y install ttf-unfonts-core
 apt-get -y install ttt
-apt-get -y install tulip
-apt-get -y install tulip-doc
 apt-get -y install udns-utils
-apt-get -y install unrar
-apt-get -y install vim
-apt-get -y install winbind
-apt-get -y install wine
-apt-get -y install wine-gecko1.4
-apt-get -y install winetricks
-apt-get -y install wireshark
-apt-get -y install zenmap
+
+
