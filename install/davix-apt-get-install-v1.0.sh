4441745589cc382f536b7ac62ad594b51d8bdbc6
AmandaLG <amandalg@gmail.com>
2014-04-20 10:03:36 -0700

    Removed p0f since it is being pulled later.

--- install/davix-apt-get-install-v1.0.sh
+++ install/davix-apt-get-install-v1.0.sh
@@ -31,6 +31,6 @@ apt-get -y install apache2-mpm-prefork mysql-client mysql-server ntp openssh-ser
 apt-get -y install chaosreader dnstop etherape geoip-database geoip-bin ggobi gnuplot googleearth-package
 apt-get -y install graphviz gwhois mrtg netcat netexpect netrw netsed nfdump ngrep nmap octave octave-image
 apt-get -y install octave-linear-algebra octave-mapping octave-nan octave-plplot octave-splines octave-tsa qtoctave
-apt-get -y install p0f pads picviz ploticus prads prelude-manager rrdtool sagan snort socat syslog-ng
+apt-get -y install pads picviz ploticus prads prelude-manager rrdtool sagan snort socat syslog-ng
 apt-get -y install tcpflow tcpreplay tcpslice tcpstat tcpxtract tshark ttt tulip tulip-doc wireshark zenmap
 
