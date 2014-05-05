6808d11fdce283d85386d4161678ccfabbaba6aa
AmandaLG <amandalg@gmail.com>
2014-05-05 10:38:46 -0700

    Revert change.

--- install/davix-install-4-menus.sh
+++ install/davix-install-4-menus.sh
@@ -39,6 +39,6 @@ cp $DPMI/davix/install/menus/applications.menu $DAVEHM/.config/menus
 # Fetch the logo and set the desktop 
 cd $DPMI/davix/logos
 cp davix-2014_white.png $DH
-gsettings set org.gnome.desktop.background primary-color 'FFFFFF'
+gsettings set org.gnome.desktop.background primary-color '#ffffff'
 gsettings set org.gnome.desktop.background picture-uri 'file:///home/davix/davix-2014_white.png'
 gsettings set org.gnome.desktop.background picture-options 'scaled'
