8582c3336ff25dbf7a67d225d750793c1a40d8c8
Raffael Marty <raffy@pixlcloud.com>
2014-05-05 11:25:59 -0700

    Turning off more servies

--- install/davix-config.sh
+++ install/davix-config.sh
@@ -10,9 +10,16 @@ echo manual >> /etc/init/ssh.override
 echo manual >> /etc/init/cups.override
 mv /etc/inetd.conf /etc/inetd.disabled
 update-rc.d tor disable
+# turn off samba
+echo manual >> /etc/init/winbind
 
-# turn off new services
+# turn off new DAVIX installed services
 # turn off snort
+update-rc.d snort disable
 # turn off pads
+update-rc.d pads disable
 # turn off prads
+update-rc.d prads disable
 # turn off logstash
+echo manual >> /etc/init/logstash.override
+echo manual >> /etc/init/logstash-web.override
