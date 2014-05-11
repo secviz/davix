2f103b66f497f53a67bc9bb4c1fb812a0adca952
AmandaLG <amandalg@gmail.com>
2014-05-11 13:10:20 -0800

    Menu item updates.

--- install/davix-install-1-apt-get.sh
+++ install/davix-install-1-apt-get.sh
@@ -53,6 +53,6 @@ apt-get -y install prelude-manager
 apt-get -y install chaosreader dnstop etherape geoip-database geoip-bin ggobi gnuplot googleearth-package
 apt-get -y install graphviz gwhois mrtg libpcap-dev netcat netexpect netrw netsed nfdump ngrep nmap octave octave-image
 apt-get -y install octave-linear-algebra octave-mapping octave-nan octave-plplot octave-splines octave-tsa qtoctave
-apt-get -y install pads picviz ploticus prads python-scapy rrdtool sagan snort socat syslog-ng
+apt-get -y install pads picviz ploticus prads python-scapy rrdtool rsyslog sagan snort socat syslog-ng
 apt-get -y install tcpdump tcpflow tcpreplay tcpslice tcpstat tcpxtract tshark ttt tulip tulip-doc wireshark zenmap
-apt-get -y install libtext-csv-perl
+apt-get -y install libtext-csv-perl 
