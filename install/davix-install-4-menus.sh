69fd62830c323628f54b5e94c9f68e73b4e5d238
Raffael Marty <raffy@pixlcloud.com>
2014-07-11 14:21:44 -0700

    new menu system

--- install/davix-install-4-menus.sh
+++ install/davix-install-4-menus.sh
@@ -15,38 +15,29 @@ export DPMI="/home/davix/davix-packages-manual-install"
 export DAVEHM="/home/davix"
 
 # Create local directories for DAVIX user
-#mkdir -p $DAVEHM/.local/share/desktop-directories
-#mkdir -p $DAVEHM/.local/share/applications
-#mkdir -p $DAVEHM/.config/menus
+mkdir -p $DAVEHM/.local/share/desktop-directories
+mkdir -p $DAVEHM/.local/share/applications
+mkdir -p $DAVEHM/.config/menus
 
 # Move the Menu Items
-# ./home/davix/.local/share/applications/
-rm -rf /usr/share/applications/davix2014*
-cp $DPMI/davix/install/menus/desktop/* /usr/share/applications
+cp $DPMI/davix/install/menus/desktop/* $DAVEHM/.local/share/applications/
 
 # Move the Menu Directories
-# ./home/davix/.local/share/desktop-directories/
-rm -rf /usr/share/desktop-directories/davix2014*
-cp $DPMI/davix/install/menus/directories/* /usr/share/desktop-directories
+cp $DPMI/davix/install/menus/directories/* $DAVEHM/.local/share/desktop-directories/
 
-# Move the Menu File
-# ./home/davix/.config/menus/
-cp /etc/xdg/menus/gnome-applications.menu /etc/xdg/menus/gnome-applications.menu.old
-cp $DPMI/davix/install/menus/applications.menu /etc/xdg/menus/gnome-applications.menu
+cp $DPMI/davix/install/menus/lxde-applications.menu $DAVEHM/.config/menus/
 
-# Set gnome classic as the default interface
-/usr/lib/lightdm/lightdm-set-defaults -s gnome-classic -m true -R false -l false
+sudo -u davix lxpanelctl restart
 
-# Fetch the logo and set the desktop 
+# put cmd-line file help in place
+cp -r $DPMI/davix/install/cmdline-help /opt/davix/
+
+# autologin / etc.
+cp $DPMI/davix/install/config/lxdm.conf /etc/lxdm/lxdm.conf
+
+# Background
 cp $DPMI/davix/logos/davix-2014_black.png $DAVEHM
-gsettings set org.gnome.desktop.background primary-color '#000000'
-gsettings set org.gnome.desktop.background picture-uri 'file:///home/davix/davix-2014_black.png'
-gsettings set org.gnome.desktop.background picture-options 'scaled'
-
-# Move the sample data
-echo "Moving sample data in place"
-mkdir -p $DH/samples
-#cp -r $DPMI/davix/install/samples/* $DH/samples/
+sed -i 's/wallpaper=.*/wallpaper=/home/davix/davix-2014_black.png/' /usr/share/lxde/pcmanfm/LXDE.conf
 
 # Sync and generate the static wiki
 git clone https://github.com/secviz/davix.wiki.git /opt/davix/wiki
