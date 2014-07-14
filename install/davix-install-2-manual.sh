36c19849216c0297856001679c6cc3f47073ad86
Raffael Marty <raffy@pixlcloud.com>
2014-07-14 13:59:13 -0700

    Updating to LogStash 1.4.2

--- install/davix-install-2-manual.sh
+++ install/davix-install-2-manual.sh
@@ -163,12 +163,12 @@ mv inetvis-0.9.3.1 $DH
 echo "Installing Logstash"
 mkdir -p $DH/logstash
 cd $DH/logstash
-wget -c https://download.elasticsearch.org/logstash/logstash/packages/debian/logstash_1.4.1-1-bd507eb_all.deb
-dpkg -i logstash_1.4.1-1*.deb
+wget -c https://download.elasticsearch.org/logstash/logstash/packages/debian/logstash_1.4.2-1-2c0f5a1_all.deb
+dpkg -i logstash_1.4.2-1*.deb
 sed -i -e 's/ -l .*/"/' /etc/init/logstash-web.conf
 sed -i -e 's/^setuid/#setuid/' /etc/init/logstash.conf
 sed -i -e 's/^setgid/#setgid/' /etc/init/logstash.conf
-mv /opt/logstash/vendor/kibana/app/dashbords/logstash.json /opt/logstash/vendor/kibana/app/dashbords/default.json 
+mv /opt/logstash/vendor/kibana/app/dashboards/logstash.json /opt/logstash/vendor/kibana/app/dashboards/default.json 
 
 # Build the base configuration file
 cat << EOF > /etc/logstash/conf.d/logstash.conf
