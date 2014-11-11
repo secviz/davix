15a81cfc9d1da2bec2b2fb1c7a3306be29d8651c
Raffael Marty <raffy@pixlcloud.com>
2014-11-11 05:12:57 -0800

    Need normal whois

--- install/davix-install-1-apt-get.sh
+++ install/davix-install-1-apt-get.sh
@@ -61,7 +61,7 @@ apt-get -y install prelude-manager
 
 ## DAVIX Packages
 apt-get -y install chaosreader dnstop etherape geoip-database geoip-bin ggobi gnuplot googleearth-package
-apt-get -y install gwhois mrtg libpcap-dev netcat netexpect netrw netsed nfdump ngrep nmap octave octave-image
+apt-get -y install whois gwhois mrtg libpcap-dev netcat netexpect netrw netsed nfdump ngrep nmap octave octave-image
 apt-get -y install octave-linear-algebra octave-mapping octave-nan octave-plplot octave-splines octave-tsa qtoctave
 apt-get -y install pads picviz ploticus prads python-scapy rrdtool rsyslog sagan snort socat syslog-ng
 apt-get -y install tcpdump tcpflow tcpreplay tcpslice tcpstat tcpxtract tshark ttt wireshark zenmap
